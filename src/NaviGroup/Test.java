ab8029ec73200b9af50d0d16263d2bf4d9ba30b2
oulaa <oulaaysoufiane@gmail.com>
2023-05-24 00:50:32 -0400

    new update

--- src/NaviGroup/Test.java
+++ src/NaviGroup/Test.java
@@ -1,4 +1,33 @@
 package NaviGroup;
 
-public class Test {
+public class scanner {
+
+    public class scanner {
+    package Quizz;
+
+import java.util.Scanner;
+
+        public class NDivide_withoutDivided_Opertaor {
+            public static void main(String[] args) {
+
+                Scanner input = new Scanner(System.in);
+
+                System.out.println(" enter the number 1 ");
+                int number2 = input.nextInt();
+
+                System.out.println(" enter the number 2");
+
+                int number3 = input.nextInt();
+                int num1 = number2;
+                int count = 0;
+                int result = 0;
+                while (number2 >= number3) {
+                    result = number2 - number3;
+                    number2 = result;
+                    count++;
+                }
+                System.out.println(num1 + " / " + number3 + " = " + count);
+            }
+        }
+    }
 }
---
+++ src/NaviGroup/finra.java
@@ -0,0 +1,23 @@
+package NaviGroup;
+
+
+public class Finra {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 5 == 0 || i % 3 == 0) {
+                if (i % 5 == 0 && i % 3 == 0) {
+                    System.out.println("FINRA");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fin");
+                } else if (i % 5 == 0) {
+
+                    System.out.println("RA");
+                }
+
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
