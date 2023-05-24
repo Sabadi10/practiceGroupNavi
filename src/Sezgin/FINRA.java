d6295384c4c703f5c36d173b484c88b8e596b413
erdogan58 <erdogansezgin58@gmail.com>
2023-05-24 12:53:57 -0400

    updated FINRA

--- src/Sezgin/FINRA.java
+++ src/Sezgin/FINRA.java
@@ -1,4 +1,23 @@
-package Sezgin;
-
 public class FINRA {
+
+    public static void printNumbers() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+
 }
+
+/*
+Write a function which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3, print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
