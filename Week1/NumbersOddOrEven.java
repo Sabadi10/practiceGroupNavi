33b36e0f664c3f59eb1c27ad6381fccabacb459c
KateKing29 <kintonkat@gmail.com>
2023-05-24 08:46:32 -0400

    moved in package week 1

--- Week1/NumbersOddOrEven.java
+++ Week1/NumbersOddOrEven.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class week1NumbersOddOrEven {
+public class NumbersOddOrEven {
        public static void main(String[] args) {
 
               Scanner input = new Scanner(System.in);
