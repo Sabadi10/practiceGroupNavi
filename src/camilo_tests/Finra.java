5375a159606b0406c44e89ff96f05ff1a52019d4
Camilo_Cydeo <Camilorojas8@knights.ucf.edu>
2023-05-24 14:53:09 -0400

    Created FINRA

---
+++ src/camilo_tests/Finra.java
@@ -0,0 +1,30 @@
+package camilo_tests;
+
+public class Finra {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 30; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 5 == 0 && i % 3 == 0) {
+                    System.out.println("FINRA");
+
+                } else if (i % 3 == 0) {
+                    System.out.println("FIN");
+
+                } else if (i % 5 == 0) {
+                    System.out.println("RA");
+                }
+
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+}
+
+/*
+Write a function which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3, print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
