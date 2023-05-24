1a1da0edc36787c913d99101f7adca21fe336eec
erdogan58 <erdogansezgin58@gmail.com>
2023-05-24 13:02:19 -0400

    Created EvenOrOdd

---
+++ src/Sezgin/EvenOrOdd.java
@@ -0,0 +1,32 @@
+package Sezgin;
+
+class EvenOrOdd {
+
+    public static void EvenOrOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        EvenOrOdd(5);
+        EvenOrOdd(6);
+
+
+
+
+    }
+}
+/*
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+ */
+
