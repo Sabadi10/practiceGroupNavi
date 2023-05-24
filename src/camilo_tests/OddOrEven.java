c2cf1738105b32bb192c0e2ca26d0dcdc3ca2bce
Camilo_Cydeo <Camilorojas8@knights.ucf.edu>
2023-05-24 18:02:15 -0400

    updated Odd or Even

--- src/camilo_tests/OddOrEven.java
+++ src/camilo_tests/OddOrEven.java
@@ -2,23 +2,6 @@ package camilo_tests;
 
 import java.util.Scanner;
 
-public class OddOrEven {
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int num = input.nextInt();
-
-        if (num % 2 == 0) {
-            System.out.println(num + " is even");
-        } else {
-            System.out.println(num + " is odd");
-        }
-
-    }
-}
-
 /*
 Numbers- Odd or even:
 Write  a method which can identify given number is even or odd
@@ -26,3 +9,20 @@ Output ex:
 identify(5); ->"Odd"
 identify(6); ->"Even"
  */
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); // user input
+        System.out.println("Enter a number: ");
+        int num = input.nextInt(); // used to read the users input
+
+        if (num % 2 == 0) { // checks if the num divisible by 2
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd"); // if remainder is not 0 then it is odd because it is not divisible by 2
+        }
+
+    }
+}
