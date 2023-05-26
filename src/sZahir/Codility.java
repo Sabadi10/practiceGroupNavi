afd4d5b6e9ca9288d54e9a4311f09f68031b8b8d
zshunay <zshunay@gmail.com>
2023-05-26 12:09:41 -0400

    coditity and numbers task update

--- src/sZahir/Codility.java
+++ src/sZahir/Codility.java
@@ -1,21 +1,31 @@
 package sZahir;
 
+/*
+Write a function that given a positive integer N prints the consecutive numbers
+from 1 to n  each on separate lines
+- if the numbers is divisible by more than one of the numbers 2,3,5 etc
+it should print Codility, Test, Coders
+
+ */
 public class Codility {
-    public static void ConsecutiveNumber(int num){
+    public static void ConsecutiveNumber(int num) {
 
-    for (int i = 1; i <= num; i++) {
-        StringBuilder output = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+             StringBuilder output = new StringBuilder();
 
 
-        if (i % 2 == 0) {
-            output.append("Codility"); // output stores the values
-        }
-        if (i % 3 == 0) {
-            output.append("Test");
-        }
-        if (i % 5 == 0) {
-            output.append("Coders");
-        }
+            if (i % 2 == 0) {
+                output.append("Codility"); // output stores the values
+
+            }
+            if (i % 3 == 0) {
+                 output.append("Test");
+
+            }
+            if (i % 5 == 0) {
+                output.append("Coders");
+
+            }
 
         if (output.length() > 0) {
             System.out.println(output);
@@ -23,28 +33,17 @@ public class Codility {
             System.out.println(i);
         }
     }
-}
+
+
+        }
+
 
 
     public static void main(String[] args) {
 
-ConsecutiveNumber(24);
+ConsecutiveNumber(90);
 
         }
 
     }
 
-
-
-/*
-Write a function that given a positive integer N prints the consecutive numbers
-from 1 to n  each on separate lines ( for loop)
-- if the numbers is divisible by more than one of the numbers 2,3,5 etc it should print Codility, Test, Coders
-
-
-Logic
-if prime return number return number // if doeesnt firt criteria
-if divisible by 2 numbers return CodilityTest ...put all in one statement
-if divisible by 3 numbers return CodilityTestCoders .... one statement
-
- */
