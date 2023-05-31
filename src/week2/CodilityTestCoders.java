335f8f771262e91b0c36626d3f6d75c8a4efa8d8
erdogan58 <erdogansezgin58@gmail.com>
2023-05-31 11:51:50 -0400

    set up CodilityTestCoders

--- src/week2/CodilityTestCoders.java
+++ src/week2/CodilityTestCoders.java
@@ -1,4 +1,59 @@
 package week2;
 
 public class CodilityTestCoders {
+
+    static int N = 45;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) {
+                System.out.print("Codility");
+                if (i % 3 == 0) {
+                    System.out.print("Test");
+                }
+
+                if (i % 5 == 0) {
+                    System.out.print("Coders");
+                }
+                System.out.println();
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.print("Test");
+                if (i % 2 == 0) {
+                    System.out.print("Codility");
+                }
+
+                if (i % 5 == 0) {
+                    System.out.print("CODERS");
+                }
+                System.out.println();
+                continue;
+            }
+
+            if (i % 5 == 0) {
+                System.out.print("CODERS");
+                if (i % 2 == 0) {
+                    System.out.print("CODILITY");
+                }
+
+                if (i % 3 == 0) {
+                    System.out.print("TEST");
+                }
+                System.out.println();
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
+
+/*
+Write a function that given a positive integer N prints the consecutive numbers
+from 1 to n  each on separate lines
+- if the numbers is divisible by more than one of the numbers 2,3,5 etc
+it should print Codility, Test, Coders
+
+ */
