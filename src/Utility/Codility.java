ad357994461c8945a3283207e87fee7adc03ce2e
Nataliia_Shulga <shulga.nataliia1@gmail.com>
2023-05-31 15:34:06 -0400

    added codility file

---
+++ src/Utility/Codility.java
@@ -0,0 +1,36 @@
+package Utility;
+
+public class Codility {
+    /*
+Write a function that given a positive integer N prints the consecutive numbers
+from 1 to n  each on separate lines. However , any number divisible by 2, 3 or 5  should  be replaced by the word Codility, Test or Coders respectively. If a number is divisible by more than one of the numbers 2,3 or 5,  it should placed by concatenation of respective words Codility, Test and Coders in this given order. For example, 2,3 and 5, should be replaced by CodilityTestCoders.
+ */
+
+    public static void main(String[] args) {
+
+        int number = 24;
+        String divisibleBy2 = "Codility";
+        String divisibleBy3 = "Test";
+        String divisibleBy5 = "Coders";
+
+        for (int i = 1; i <=number ; i++) {
+            if( i % 2 ==0 && i % 3 == 0 && i % 5 == 0){
+                System.out.println(divisibleBy2+divisibleBy3+divisibleBy5);
+            }else if(i % 2 ==0 && i % 3 == 0){
+                System.out.println(divisibleBy2+divisibleBy3);
+            }else if(i % 2 == 0 && i % 5 == 0){
+                System.out.println(divisibleBy2+divisibleBy5);
+            }else if( i % 3 == 0 & i % 5 == 0){
+                System.out.println(divisibleBy3+divisibleBy5);
+            }else if(i % 2 == 0 && i % 3 !=0 & i % 5!=0){
+                System.out.println(divisibleBy2);
+            }else if(i %3 == 0 && i %2 !=0 && i %5 !=0){
+                System.out.println(divisibleBy3);
+            }else if(i % 5 ==0 && i %2 !=0 & i % 3 !=0){
+                System.out.println(divisibleBy5);
+            }
+
+
+        }
+    }
+}
