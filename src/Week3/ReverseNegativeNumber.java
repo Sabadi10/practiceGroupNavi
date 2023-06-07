9f7efe6c4986a66285eab69baba6918bca3584db
KateKing29 <kintonkat@gmail.com>
2023-06-07 10:49:32 -0400

    added 2 task

---
+++ src/Week3/ReverseNegativeNumber.java
@@ -0,0 +1,19 @@
+package Week3;
+/*
+Write a return method that can reverse negative number and return it as int.
+First, we find the remainder of the given number by using the modulo (%) operator.
+Multiply the variable reverse by 10 and add the remainder into it.
+Divide the number by 10. Repeat the above steps until the number becomes 0.
+*/
+public class ReverseNegativeNumber {
+    public static void main(String[] args){
+            int number = -654321, reverse = 0;
+            while(number != 0)
+            {
+                int remainder = number%10;
+                reverse = reverse*10+remainder;
+                number = number/10;
+            }
+            System.out.println("The reverse of the given number is: " + reverse);
+        }
+    }
