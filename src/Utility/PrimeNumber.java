163e7b3612277578eb971e27ddc435d8803a304e
Nataliia_Shulga <shulga.nataliia1@gmail.com>
2023-06-14 16:39:20 -0400

    updated PrimeNumber file

--- src/Utility/PrimeNumber.java
+++ src/Utility/PrimeNumber.java
@@ -9,21 +9,26 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
 
-        primeNumber(12);
+        primeNumber(11);
 
 
     }
 
     public static void primeNumber (int num){
 
-        for (int i = 0; i < num; i++) {
+        if(num < 2){
+            System.out.println(num + " is not a prime number");
 
-        }
-            if(num % 2 ==0 || num < 2){ //all numbers less equal 1 is not considered as prime numbers
+        for (int i = 0; i < num; i++) {
+            if(num % i ==0 ){ //all numbers less equal 1 is not considered as prime numbers
                 System.out.println(num + " is not a prime number");
             }else{
                 System.out.println(num + " is a prime number");
             }
+
+            }
+        }
+
 
 
     }
