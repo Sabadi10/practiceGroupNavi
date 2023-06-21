32f2b07833a21aee0be6ba1a07bd9a91fdc14325
ATM <124079178+ATM192@users.noreply.github.com>
2023-06-21 17:33:04 -0400

    commit

---
+++ src/Tasks/MaximumNumInArray.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class MaximumNumInArray {
+    public static void main(String[] args) {
+
+    }
+    public static int MaxNum(){
+        int max = 0;
+        return max;
+    }
+}
