934576685fdc24e39f0d16d5057f16002f9953cd
Nataliia_Shulga <shulga.nataliia1@gmail.com>
2023-06-21 15:41:16 -0400

    created StringReverse file

---
+++ src/Utility/ReverseString.java
@@ -0,0 +1,19 @@
+package Utility;
+
+public class ReverseString {
+
+    //write a return method that can reverse String
+    //Ex: Reverse("ABCD")==>DCBA
+
+    public static void reverseString(String str){
+        String reverse = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            reverse+=str.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+
+    public static void main(String[] args) {
+        reverseString("ABCD1234");
+    }
+}
