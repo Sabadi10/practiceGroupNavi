60e4b415cd42258d51f99f392d527eae2fe44f9e
erdogan58 <erdogansezgin58@gmail.com>
2023-06-21 16:39:04 -0400

    updated UniqueValues

--- src/week4/UniqueValues.java
+++ src/week4/UniqueValues.java
@@ -1,27 +1,38 @@
 package week4;
 
-public class UniqueValues {
-    public static void main(String[] args)
-    {
-        String str = "GYFTGFSAHFGAHFSG";
+import java.util.Scanner;
 
-        for (int i = 0; i < str.length(); i++)
-        {
-            int result = 0;
-            for (int j = 0; j < str.length(); j++)
-            {
-                // checking if two characters are equal
-                if (str.charAt(i) == str.charAt(j) && i != j)
-                {
-                    result = 1;
-                    break;
-                }
+public class UniqueValues {
+    public static void main(String[] args) {
+
+
+        Scanner kb = new Scanner(System.in);
+        String word = "";
+
+        System.out.println("Enter a word: ");
+        word = kb.nextLine();
+
+        uniqueCharacters(word);
+    }
+
+
+    public static void uniqueCharacters(String test){
+        String temp = "";
+        for (int i = 0; i < test.length(); i++){
+            if (temp.indexOf(test.charAt(i)) == - 1){
+                // searches this string and returns the index of the first occurrence of the specified substring.
+                temp = temp + test.charAt(i);
             }
-            if (result == 0)
-                System.out.print(str.charAt(i));
         }
+
+        System.out.println(temp + " ");
+
     }
 }
+
+
+
+
 /*
 3.String -- Find the unique
 Write a return  method that can find the unique characters from the String
