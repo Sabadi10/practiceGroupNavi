cbe87f3876dd7d60c9bc9f950dc126eb8562514e
Camilo_Cydeo <Camilorojas8@knights.ucf.edu>
2023-06-21 13:12:38 -0400

    Created ReverseString

---
+++ src/camilo_tests/week05/ReverseString.java
@@ -0,0 +1,28 @@
+package camilo_tests.week05;
+
+public class ReverseString {
+
+    /*
+                2)  String - Reverse
+                    Write a return method that can reverse String
+                    Ex: Reverse("ABCD"); ==> DCBA (edited)
+     */
+
+    public static void reverseString (String word){
+
+        String reverse = "";
+        for (int i = word.length()-1; i >=0 ; i--) {
+            reverse+=word.charAt(i);
+        }
+        System.out.println(reverse);
+
+        }
+
+    public static void main(String[] args) {
+        reverseString("ABCDEFG");
+
+        }
+    }
+
+
+
