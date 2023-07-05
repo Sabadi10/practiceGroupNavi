d534cba7b283573098319a319a114084ec2db900
erdogan58 <erdogansezgin58@gmail.com>
2023-07-05 17:18:42 -0400

    created StringPasswordValidation

---
+++ src/week6/StringPasswordValidation.java
@@ -0,0 +1,13 @@
+package week6;
+/*
+    1) String - Password Validation Task
+Write a return method that can verify if a password is valid or not. Requirements:
+1. Password MUST be at least 6 characters and should not contain space
+Password should at least contain one upper case letter
+Password should at least contain one lowercase letter
+Password should at least contain one special characters
+Password should at least contain a digit
+if all requirements above are met, the method returns true, otherwise returns false
+ */
+public class StringPasswordValidation {
+}
