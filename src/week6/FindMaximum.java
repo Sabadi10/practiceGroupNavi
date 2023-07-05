61fa764fa2fc6d69fea8d4b14173ff93ef9c174d
erdogan58 <erdogansezgin58@gmail.com>
2023-07-05 15:03:07 -0400

    updated FindMaximum

--- src/week6/FindMaximum.java
+++ src/week6/FindMaximum.java
@@ -32,19 +32,4 @@ public class FindMaximum {
  3) Array - Find Maximum
 Write a method that can find the maximum number from an int Array
  */
-/*
-    int[] numbers = {100, 20, 500, 40, -10, 30};
 
-    int max = numbers[0]; // assume that first element is the maximum number
-
-        for (int i = 1; i < numbers.length; i++) { // i: 1, 2, 3, 4...
-
-        if(numbers[i] > max){ // compares the element of the array with current maximum number
-        max = numbers[i]; // replace the current maximum number
-        }
-
-        }
-
-
-        System.out.println("max = " + max);
-*/
