5be66b01ce9d2ac15c2d3fda884cbdbbaba64d4a
erdogan58 <erdogansezgin58@gmail.com>
2023-07-05 17:15:47 -0400

    updated FindMinumum

--- src/week7/FindMinumum.java
+++ src/week7/FindMinumum.java
@@ -1,4 +1,30 @@
 package week7;
+import week6.FindMaximum;
+/*
+    1) Array - Find Minimum
+            Write a method that can find the maximum number from an int Array
 
+    */
 public class FindMinumum {
+    public static int FindMinumum (int numbers[] ) {
+
+        int min = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Min number is "+ FindMinumum( new int[]{120, 200, -15, 43, -125, 1903}));
+    }
+
 }
+
+
