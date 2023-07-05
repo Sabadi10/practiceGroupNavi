646cecb195e4ebc150b254795404e3143dfa5d74
ATM <124079178+ATM192@users.noreply.github.com>
2023-07-05 16:55:11 -0400

    New Commit

--- src/Tasks/SortAscendingArray.java
+++ src/Tasks/SortAscendingArray.java
@@ -1,7 +1,11 @@
 package Tasks;
 
 import java.util.Arrays;
-
+/*    2) Array - Sort Ascending
+Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+Ex:
+int[] arr = {10, 9, 8, 7};
+arr = Sort(arr);==>{ 7, 8, 9, 10};*/
 public class SortAscendingArray {
     public static void main(String[] args) {
         int arr1[] = {545, 58, 241};
--- src/Tasks/SortDescending.java
+++ src/Tasks/SortDescending.java
@@ -3,6 +3,10 @@ package Tasks;
 import java.util.Arrays;
 
 public class SortDescending {
+  /*    3) Array - Sort Descending
+Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
+Ex:  int[] arr = {10,20,7, 8, 90};
+                 arr = Sort(arr);  ==>  {90, 20, 10, 8, 7};*/
     public static void main(String[] args) {
         int arr1[] = {545, 58, 241};
         System.out.println(Arrays.toString(sortDescending(arr1)));
