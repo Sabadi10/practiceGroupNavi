f62bcf77372d61a7f6deead17ddfe351985cd97e
KateKing29 <kintonkat@gmail.com>
2023-07-12 16:57:41 -0400

    added 1 task

---
+++ src/Week8/ArrayMoveZerosToTheEnd.java
@@ -0,0 +1,24 @@
+package Week8;
+import java.util.Arrays;
+
+/*  Create a method that will accept an int array and return an array with all the zeros moved to the end
+        Ex:             input:  {1, 0, 2, 0, 3, 0, 4, 0};
+                        output: [1, 2, 3, 4, 0, 0, 0, 0]
+*/
+public class ArrayMoveZerosToTheEnd {
+    public static int[] moveZeros(int... nums){
+
+        int[] moved = new int[nums.length];
+        int index = 0;
+
+        for(int each : nums){
+            if(each != 0){
+                moved[index++] = each;
+            }
+        }
+        return moved;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(moveZeros(1, 0, 2, 0, 3, 0, 4, 0)));
+    }
+}
