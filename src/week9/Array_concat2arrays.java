5864be9b00e332df632595046d00d1f8f2cfc6a8
erdogan58 <erdogansezgin58@gmail.com>
2023-07-26 12:41:53 -0400

    created Array_concat2arrays

---
+++ src/week9/Array_concat2arrays.java
@@ -0,0 +1,27 @@
+package week9;
+/*
+  Array_concat2Arrays
+Write a return method that can concatenate two arrays
+ */
+
+import java.util.Arrays;
+
+public class Array_concat2arrays {
+
+    public static int[] concatenateArrays(int[] arr, int[] arr1) {
+        int[] result = new int[arr.length + arr1.length];
+
+        System.arraycopy(arr, 0, result, 0, arr1.length);
+        System.arraycopy(arr1, 0, result, arr1.length, arr1.length);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,3,5,7};
+        int[] array1 = {2,4,6,8};
+        int[] result = concatenateArrays(array, array1);
+        System.out.println(Arrays.toString(result)); // Output: [1, 2, 3, 4, 5, 6,7,8]
+    }
+}
+
