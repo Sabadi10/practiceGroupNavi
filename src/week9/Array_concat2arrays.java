10256bf01b81b7b2cb8ec615885526ecbf98d9fd
erdogan58 <erdogansezgin58@gmail.com>
2023-07-26 16:14:15 -0400

    updated Array_concat2arrays

--- src/week9/Array_concat2arrays.java
+++ src/week9/Array_concat2arrays.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Array_concat2arrays {
 
-    public static int[] concatenateArrays(int[] arr, int[] arr1) {
+    public static int[] concatArrays(int[] arr, int[] arr1) {
         int[] result = new int[arr.length + arr1.length];
 
         System.arraycopy(arr, 0, result, 0, arr.length);
@@ -20,7 +20,7 @@ public class Array_concat2arrays {
     public static void main(String[] args) {
         int[] array = {1,3,5,7,9};
         int[] array1 = {2,4,6,8};
-        int[] result = concatenateArrays(array, array1);
+        int[] result = concatArrays(array, array1);
         System.out.println(Arrays.toString(result)); // Output: [1, 3, 5, 7, 9, 2, 4, 6, 8]
     }
 }
