bdf8551c19d165fcf81c4799bbb99699073a9205
KateKing29 <kintonkat@gmail.com>
2023-07-26 20:33:45 -0400

    update 3 task

--- src/Week9/List_RemoveSomeValues.java
+++ src/Week9/List_RemoveSomeValues.java
@@ -7,6 +7,7 @@ import java.util.List;
 //Given a list of Integers 1, 2, 3, 4, 5, 6, 101, 200, 300....etc. remove all values greater than 100
 public class List_RemoveSomeValues {
 
+
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 101, 200, 300));
