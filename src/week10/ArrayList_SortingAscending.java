505ab58efcdae4f504eba23eacb917f37087ce3d
erdogan58 <erdogansezgin58@gmail.com>
2023-08-02 12:02:47 -0400

    created ArrayList_SortingAscending

---
+++ src/week10/ArrayList_SortingAscending.java
@@ -0,0 +1,36 @@
+package week10;
+
+import java.util.ArrayList;
+
+public class ArrayList_SortingAscending {
+    public static void SortAscending(ArrayList<Integer> list) {
+        int nums = list.size();
+        for (int i = 0; i < nums - 1; i++) {
+            for (int j = 0; j < nums - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    // Swap elements if they are in the wrong order
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+        public static void main(String[] args) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(15);
+            list.add(13);
+            list.add(7);
+            list.add(5);
+            list.add(83);
+
+            System.out.println("Giving ArrayList: " + list);
+
+            // Sort the ArrayList in ascending order
+            SortAscending(list);
+
+            System.out.println("Sorted ArrayList: " + list);
+
+    }
+}
+
