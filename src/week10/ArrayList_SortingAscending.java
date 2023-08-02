d90c2d12f1ff57978282cdc572f93f0049d7ee6e
erdogan58 <erdogansezgin58@gmail.com>
2023-08-02 12:04:57 -0400

    updated ArrayList_SortingAscending

--- src/week10/ArrayList_SortingAscending.java
+++ src/week10/ArrayList_SortingAscending.java
@@ -1,6 +1,9 @@
 package week10;
-
 import java.util.ArrayList;
+/*
+  ArrayList - sorting in ascending
+Write a method that can sort the ArrayList in Ascending order without using the sort method.
+ */
 
 public class ArrayList_SortingAscending {
     public static void SortAscending(ArrayList<Integer> list) {
